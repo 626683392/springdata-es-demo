ce0a4ed478003cf1f5dab146b00be6c7ab40e79f
yupingtao <626683392@qq.com>
2019-11-26 23:24:44 +0800

    添加crud代码

---
+++ src/main/java/com/dataes/example/demo/dao/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.dataes.example.demo.dao;
+ 
+/**
+ * Created by 19130 on 2018/5/16.
+ */
+import com.dataes.example.demo.entity.Employee;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Component;
+/**
+ * @author linzhiqiang
+ */
+@Component
+public interface EmployeeRepository extends ElasticsearchRepository<Employee,String>{
+ 
+    /**
+     * 查询雇员信息
+     * @param id
+     * @return
+     */
+    Employee queryEmployeeById(String id);
+}
