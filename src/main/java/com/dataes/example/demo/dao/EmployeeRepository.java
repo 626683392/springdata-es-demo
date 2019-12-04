25008bdd46232ed781ab4eb5d3f0b5ad15d2c2b0
yupingtao <626683392@qq.com>
2019-12-04 23:48:29 +0800

    添加crud代码

--- src/main/java/com/dataes/example/demo/dao/EmployeeRepository.java
+++ src/main/java/com/dataes/example/demo/dao/EmployeeRepository.java
@@ -7,7 +7,6 @@ import com.dataes.example.demo.entity.Employee;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Component;
 /**
- * @author linzhiqiang
  */
 @Component
 public interface EmployeeRepository extends ElasticsearchRepository<Employee,String>{
